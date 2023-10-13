8afa7c5b188470678db2ff72c8eb4170a667508f
Matt Mullen <matt.mullen@sophos.com>
2023-10-13 12:32:56 -0400

    add license header

--- sophosfirewall_python/firewallapi.py
+++ sophosfirewall_python/firewallapi.py
@@ -1,8 +1,14 @@
-"""firewallapi.py - Module for working with the Sophos Firewall API
-
-https://docs.sophos.com/nsg/sophos-firewall/19.5/API/index.html
-
 """
+firewallapi.py - Module for working with the Sophos Firewall API
+
+Copyright 2023 Sophos Ltd.  All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing
+permissions and limitations under the License.
+"""
+
 import os
 import re
 from ipaddress import IPv4Network, IPv4Address
