4ae3ee92d1a3681455ea7e7cfc1e59c99072b919
Matt Mullen <matt.mullen@sophos.com>
2024-08-26 11:04:07 -0400

    feat: - Added ability to update firewall rules - Added argument for position (top, bottom, before, after) when creating rules - Added argument for status (enabled/disabled) when creating/updating rules

--- sophosfirewall_python/firewallrule.py
+++ sophosfirewall_python/firewallrule.py
@@ -6,7 +6,7 @@ Unless required by applicable law or agreed to in writing, software distributed 
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing
 permissions and limitations under the License.
 """
-
+from sophosfirewall_python.utils import Utils
 
 class FirewallRule:
     """Class for working with firewall rule(s)."""
@@ -14,7 +14,7 @@ class FirewallRule:
     def __init__(self, api_client):
         self.client = api_client
 
-    def get(self, name, operator):
+    def get(self, name, operator="="):
         """Get firewall rule(s)
 
         Args:
@@ -35,7 +35,10 @@ class FirewallRule:
 
         Keyword Args:
             rulename(str): Name of the firewall rule
-            after_rulename(str): Name of the rule to insert this rule after
+            status(str): Enable/Disable
+            position(str): Where the rule should be positioned (top/bottom/after/before)
+            after_rulename(str): Name of the rule to insert this rule after if position = after
+            before_rulename(str): Name of the rule to insert this rule before if position = before
             action(str): Accept, Drop, Reject
             description(str): Rule description
             log(str): Enable, Disable
@@ -49,5 +52,105 @@ class FirewallRule:
         """
         resp = self.client.submit_template(
             "createfwrule.j2", template_vars=rule_params, debug=debug
+        )
+        return resp
+
+    def update(self, name, rule_params, debug):
+        """Update a firewall rule.
+
+        Args:
+            name(str): Name of the firewall rule to be updated
+            rule_params (dict): Configuration parmeters for the rule, see Keyword Args for supported parameters.
+
+        Keyword Args:
+            status(str): Enable/Disable
+            position(str): Where the rule should be positioned (top/bottom/after/before)
+            after_rulename(str, optional): Name of the rule to insert this rule after if position = after
+            before_rulename(str, optional): Name of the rule to insert this rule before if position = before
+            action(str): Accept, Drop, Reject
+            description(str): Rule description
+            log(str): Enable, Disable
+            src_zones(list): Name(s) of the source zone(s)
+            dst_zones(list): Name(s) of the destination zone(s)
+            src_networks(list): Name(s) of the source network(s)
+            dst_networks(list): Name(s) of the destination network(s)
+            service_list(list): Name(s) of service(s)
+        Returns:
+            dict: XML response converted to Python dictionary
+        """
+        updated_rule_params = dict(rulename=name)
+
+        # Get the existing rule
+        exist_rule = self.get(name=name)["Response"]["FirewallRule"]
+
+        if rule_params.get("action"):
+            updated_rule_params["action"] = rule_params.get("action")
+        else:
+            updated_rule_params["action"] = exist_rule["NetworkPolicy"]["Action"]
+        
+        if rule_params.get("description"):
+            updated_rule_params["description"] = rule_params.get("description")
+        else:
+            updated_rule_params["description"] = exist_rule["Description"]
+
+        if rule_params.get("status"):
+            updated_rule_params["status"] = rule_params.get("status")
+
+        if rule_params.get("position"):
+            updated_rule_params["position"] = rule_params.get("position")
+
+        if rule_params.get("after_rulename"):
+            updated_rule_params["after_rulename"] = rule_params.get("after_rulename")
+        
+        if rule_params.get("before_rulename"):
+            updated_rule_params["before_rulename"] = rule_params.get("before_rulename")
+
+        if rule_params.get("log"):
+            updated_rule_params["log"] = rule_params.get("log")
+        else:
+            updated_rule_params["log"] = exist_rule["NetworkPolicy"]["LogTraffic"]
+
+        if rule_params.get("src_zones"):
+            updated_rule_params["src_zones"] = rule_params.get("src_zones")
+        else:
+            if "SourceZones" in exist_rule["NetworkPolicy"]:
+                updated_rule_params["src_zones"] = Utils.ensure_list(exist_rule["NetworkPolicy"]["SourceZones"]["Zone"])
+            else:
+                updated_rule_params["src_zones"] = None
+
+        if rule_params.get("dst_zones"):
+            updated_rule_params["dst_zones"] = rule_params.get("dst_zones")
+        else:
+            if "DestinationZones" in exist_rule["NetworkPolicy"]:
+                updated_rule_params["dst_zones"] = Utils.ensure_list(exist_rule["NetworkPolicy"]["DestinationZones"]["Zone"])
+            else:
+                updated_rule_params["dst_zones"] = None
+
+        if rule_params.get("src_networks"):
+            updated_rule_params["src_networks"] = rule_params.get("src_networks")
+        else:
+            if "SourceNetworks" in exist_rule["NetworkPolicy"]:
+                updated_rule_params["src_networks"] = Utils.ensure_list(exist_rule["NetworkPolicy"]["SourceNetworks"]["Network"])
+            else:
+                updated_rule_params["src_networks"] = None
+
+        if rule_params.get("dst_networks"):
+            updated_rule_params["dst_networks"] = rule_params.get("dst_networks")
+        else:
+            if "DestinationNetworks" in exist_rule["NetworkPolicy"]:
+                updated_rule_params["dst_networks"] = Utils.ensure_list(exist_rule["NetworkPolicy"]["DestinationNetworks"]["Network"])
+            else:
+                updated_rule_params["dst_networks"] = None
+
+        if rule_params.get("service_list"):
+            updated_rule_params["service_list"] = rule_params.get("service_list")
+        else:
+            if "Services" in exist_rule["NetworkPolicy"]:
+                updated_rule_params["service_list"] = Utils.ensure_list(exist_rule["NetworkPolicy"]["Services"]["Service"])
+            else:
+                updated_rule_params["service_list"] = None
+
+        resp = self.client.submit_template(
+            "updatefwrule.j2", template_vars=updated_rule_params, debug=debug
         )
         return resp
